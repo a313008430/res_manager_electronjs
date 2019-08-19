84ad471fdc7ab31a2443f85037f0033feff61ee9
xia <313008430@qq.com>
2019-08-19 18:03:19 +0800

    代码备份

--- web/src/DataManager.ts
+++ web/src/DataManager.ts
@@ -1,12 +1,53 @@
 /**
  * 数据管理器
  */
+
+interface resObj {
+    /** 资源名称 */
+    resName: string,
+    /** 类型 */
+    type: number,
+    /** 路径 */
+    path: string,
+    /** 注释 */
+    note?: string
+}
+
 export default class DataManager {
 
     /**根目录 */
     root: string | undefined;
+    /** 所有的类型 */
+    allType: Set<string>;
+    /** 组列表 */
+    groupList: Map<string, Map<string, resObj>>;
 
     constructor() {
+        this.groupList = new Map();
+        this.allType = new Set();
+    }
 
+    /**
+     * 添加一组
+     * @param name 组名称
+     */
+    addGroup(name: string) {
+        this.groupList.set(name, new Map());
+    }
+
+    /**
+     * 删除一组
+     * @param name 组名称
+     */
+    clearGroup(name: string) {
+        let group = this.groupList.get(name) as Map<string, resObj>;
+
+        if (!group) return;
+
+        if (group.size) {//如果有数据是否删除
+            this.groupList.delete(name);
+        } else {//直接删除
+
+        }
     }
 }
--- web/src/ViewLogic.ts
+++ web/src/ViewLogic.ts
@@ -8,11 +8,17 @@ export default class ViewLogic {
 
     /** 上传按钮 */
     private browseBtn: HTMLInputElement;
+    /** 组列表节点 */
+    private groupListNode: HTMLDListElement;
+    /** 添加组按钮 */
+    private addGroupBtn: HTMLElement;
+    /** 我的数据列表 */
+    private myListNode: HTMLDListElement;
 
     /** 数据管理 */
     private dataManager: DataManager;
     /** 根目录列表 */
-    private rootResList: {
+    private rootResList: Map<string, {
         /** 路径 */
         path: string,
         /** 名称 */
@@ -21,14 +27,20 @@ export default class ViewLogic {
         type: string,
         /** 大小 */
         size: number
-    }[];
+    }>;
 
     constructor() {
+
+        this.rootResList = new Map();
 
         this.dataManager = new DataManager();
 
         this.browseBtn = document.querySelector('#browse') as HTMLInputElement;
+        this.groupListNode = document.querySelector('#groupListNode') as HTMLDListElement;
+        this.addGroupBtn = document.querySelector('#addGroupBtn') as HTMLDListElement;
+        this.myListNode = document.querySelector('#myListNode') as HTMLDListElement;
 
+        //点击事件
         this.browseBtn.addEventListener('change', (e: Event) => {
 
             let file: any = (this.browseBtn.files as FileList).item(0) as File;
@@ -49,6 +61,18 @@ export default class ViewLogic {
                     this.dataManager.root = rootPath;
                     this.readDir(rootPath);
                     this.drawRootList();
+
+                    //检测导入的json文件内容
+                    let resData = fs.readFileSync(file.path).toString();
+                    if (resData.length) {
+                        resData = JSON.parse(resData);
+
+                    } else {
+                        console.log('json文件为空');
+                        this.addGroup('init', false);
+                    }
+
+
                 } else {
                     alert('root path is error!')
                 }
@@ -59,13 +83,42 @@ export default class ViewLogic {
             // this.setRootPath(file)
             console.log(file);
         })
+
+
+        //添加组
+        this.addGroupBtn.addEventListener('click', ()=>{
+           var a =  prompt("请输入您的名字","Bill Gates");
+           console.log(a)
+        })
+    }
+
+    /**
+     * 添加分组
+     * @param name 组名字
+     * @param type 是否可修改和删除 true 可修改 false 不可修改
+     */
+    private addGroup(name: string, type: boolean) {
+        if (this.dataManager.groupList.has(name)) {
+            alert('分组已经存在');
+            return;
+        }
+        this.dataManager.addGroup(name);
+
+        let node = document.createElement('DD');
+        node.className = "tag g1 is-white";
+        node.innerHTML = `<input ${type ? '' : 'disabled'} class="input is-small groupName" type="text" placeholder="Group name" value=${name}>
+        ${type ? ' <button class="delete is-small"></button>' : ''}`;
+
+        this.groupListNode.appendChild(node);
     }
 
     /**
      * 清除列表初始化等
      */
     private clear() {
-        this.rootResList = [];
+        this.rootResList.clear();
+        this.groupListNode.innerHTML = '';
+        this.myListNode.innerHTML = '';
     }
 
     /**
@@ -84,18 +137,19 @@ export default class ViewLogic {
         console.log(this.rootResList)
         let html = '',
             list = this.rootResList;
-        for (let x = list.length - 1; x > -1; x--) {
-            html += `<dd>
-            <div class="g0 resName">${list[x].name}</div>
-            <div class="g0 resType select is-small">
-                <select disabled>
-                    <option>${list[x].type}</option>
-                </select>
-            </div>
-            <div class="g1">${list[x].path}</div>
-            <div class="g0 size_me">${list[x].size}</div>
-        </dd>`
-        }
+
+        list.forEach((val, key) => {
+            html += `<dd data-id=${key} data-type=${val.type}>
+                <div class="g0 resName">${val.name}</div>
+                <div class="g0 resType select is-small">
+                    <select disabled>
+                        <option>${val.type}</option>
+                    </select>
+                </div>
+                <div class="g1">${this.dataManager.root + val.path} <a class="delete is-small"></a></div>
+                <div class="g0 size_me">${this.convertFileSize(val.size)}</div>
+            </dd>`
+        })
 
         let rootListNode = document.querySelector('#rootList') as HTMLDivElement;
         rootListNode.innerHTML = html;
@@ -118,15 +172,29 @@ export default class ViewLogic {
                     this.readDir(path + '/' + files[x]);
                 } else if (stats.isFile()) {//是文件
                     let name = files[x].replace('_', '').split('.');
-                    this.rootResList.push({
+                    this.dataManager.allType.add(name[1]);//重复一直添加类型 保证类型唯一性
+
+                    let myPath = (path + '/' + files[x]).replace(this.dataManager.root + '', '');
+                    this.rootResList.set(myPath, {
                         name: name.join('_'),
-                        path: path + '/' + files[x],
-                        type: name[0],
+                        path: myPath,
+                        type: name[1],
                         size: stats.size
                     });
                 }
             }
         }
 
+    }
+
+    /** 文件大小值转换 */
+    private convertFileSize(size: number): string {
+        size = size / 1000;
+        if (size < 1024) {
+            return size.toFixed(2) + 'KB';
+        } else {
+            size = size / 1000;
+            return size.toFixed(2) + 'MB';
+        }
     }
 }
